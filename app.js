7edffdb1635930f13409565a36c86755eec18d5f
jesus.arzate <jesus.arzate@uabc.edu.mx>
2024-07-25 18:32:40 -0700

    ADD: Update app.js, add edad

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const port = 3000;
 app.get('/saludo', (req, res) => {
   const nombreBase64 = req.query.nombre;
   const apellidoBase64 = req.query.apellido;
+  const edadBase64 = req.query.edad;
 
-  if (nombreBase64 && apellidoBase64) {
+  if (nombreBase64 && apellidoBase64 && edadBase64) {
     // Decodifica los parámetros de Base64
     const nombre = base64url.decode(nombreBase64);
     const apellido = base64url.decode(apellidoBase64);
-    res.send(`¡Hola, ${nombre} ${apellido}!`);
+    const edad = base64url.decode(edadBase64);
+    res.send(`¡Hola, ${nombre} ${apellido} de ${edad} años de edad, Bienvenido al sistema`);
+
   } else {
-    res.send('Por favor, proporciona ambos parámetros "nombre" y "apellido" codificados en Base64.');
+    res.send('Por favor, proporciona ambos parámetros "nombre", "apellido" y "edad" codificados en Base64.');
   }
 });
 
